a6c34cc6bbba98a5b283fad19ee6c6e76ef95ca9
Manuel Quarneti <manuelquarneti@gmail.com>
2022-07-12 02:11:44 +0200

    👷 Optimize customtkinter bundling

--- make_exe.py
+++ make_exe.py
@@ -6,6 +6,6 @@ PyInstaller.__main__.run([
     '--noconfirm',
     '--onedir',
     '--windowed',
-    '--add-data=.venv/lib/python3.10/site-packages/customtkinter:customtkinter/',
+    '--add-data=.venv/lib/python3.10/site-packages/customtkinter/assets/themes/blue.json:customtkinter/assets/themes/',
     '--add-data=.venv/lib/python3.10/site-packages/minecraft_launcher_lib/version.txt:minecraft_launcher_lib/',
 ])
