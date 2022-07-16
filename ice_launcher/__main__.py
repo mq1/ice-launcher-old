ce83ad0c5e4366dba6fc80464fc4043cb2375ec6
Manuel Quarneti <manuelquarneti@gmail.com>
2022-07-16 18:49:44 +0200

    ♻️ Start porting from pack to grid

--- ice_launcher/__main__.py
+++ ice_launcher/__main__.py
@@ -16,54 +16,57 @@ class App(CTk):
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-        self.navigator = CTkFrame(width=180, corner_radius=0, master=self)
-        self.navigator.pack(fill="y", side="left")
+        # configure grid layout (2x1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.navigator = CTkFrame(master=self, width=180, corner_radius=0)  # type: ignore
+        self.navigator.grid(row=0, column=0, sticky="nswe")
 
         self.news_button = CTkButton(
             master=self.navigator, text="News", command=self.open_news
         )
-        self.news_button.pack(pady=(20, 10), padx=20, side="top")
+        self.news_button.grid(row=0, column=0, pady=(20, 10), padx=20)
 
         self.accounts_button = CTkButton(
             master=self.navigator, text="Accounts", command=self.open_accounts
         )
-        self.accounts_button.pack(pady=10, padx=20, side="top")
+        self.accounts_button.grid(row=1, column=0, pady=10, padx=20)
 
         self.settings_button = CTkButton(
             master=self.navigator, text="Settings", command=self.open_settings
         )
-        self.settings_button.pack(pady=10, padx=20, side="top")
+        self.settings_button.grid(row=2, column=0, pady=10, padx=20)
+
+        # empty row as spacing
+        self.navigator.grid_rowconfigure(3, weight=1)
 
         self.about_button = CTkButton(
             master=self.navigator, text="About", command=self.open_about
         )
-        self.about_button.pack(pady=(10, 20), padx=20, side="bottom")
+        self.about_button.grid(row=4, column=0, pady=(10, 20), padx=20)
 
-        self.main_frame = News(master=self)
-        self.main_frame.pack(fill="both", expand=True, pady=20, padx=20)
+        self.open_news()
 
     def on_closing(self, event=0):
         self.destroy()
 
     def open_news(self):
-        self.main_frame.destroy()
         self.main_frame = News(master=self)
-        self.main_frame.pack(fill="both", expand=True, pady=20, padx=20)
+        self.main_frame.grid(row=0, column=1, pady=20, padx=20, sticky="nswe")
 
     def open_accounts(self):
-        self.main_frame.destroy()
         self.main_frame = Accounts(master=self)
-        self.main_frame.pack(fill="both", expand=True, pady=20, padx=20)
+        self.main_frame.grid(row=0, column=1, pady=20, padx=20, sticky="nswe")
 
     def open_settings(self):
-        self.main_frame.destroy()
         self.main_frame = Settings(master=self)
-        self.main_frame.pack(fill="both", expand=True, pady=20, padx=20)
+        self.main_frame.grid(row=0, column=1, pady=20, padx=20, sticky="nswe")
 
     def open_about(self):
         self.main_frame.destroy()
         self.main_frame = About(master=self)
-        self.main_frame.pack(fill="both", expand=True, pady=20, padx=20)
+        self.main_frame.grid(row=0, column=1, pady=20, padx=20, sticky="nswe")
 
     def button_event(self):
         print("Button pressed")
