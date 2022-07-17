5cc6400b8ddeb4c9beca9661eb4b41ee47c020dd
Manuel Quarneti <manuelquarneti@gmail.com>
2022-07-17 15:17:42 +0200

    ✨ Add instances view

--- ice_launcher/__main__.py
+++ ice_launcher/__main__.py
@@ -1,4 +1,5 @@
 from customtkinter import CTk, CTkButton, CTkFrame
+from ice_launcher.instances import Instances
 from ice_launcher.news import News
 from ice_launcher.settings import Settings
 from ice_launcher.about import About
@@ -23,33 +24,42 @@ class App(CTk):
         self.navigator = CTkFrame(master=self, width=180, corner_radius=0)  # type: ignore
         self.navigator.grid(row=0, column=0, sticky="nswe")
 
+        self.instances_button = CTkButton(
+            master=self.navigator, text="Instances", command=self.open_instances
+        )
+        self.instances_button.grid(row=0, column=0, pady=(20, 10), sticky="nswe")
+
         self.news_button = CTkButton(
             master=self.navigator, text="News", command=self.open_news
         )
-        self.news_button.grid(row=0, column=0, pady=(20, 10), padx=20)
+        self.news_button.grid(row=1, column=0, pady=10, padx=20)
 
         self.accounts_button = CTkButton(
             master=self.navigator, text="Accounts", command=self.open_accounts
         )
-        self.accounts_button.grid(row=1, column=0, pady=10, padx=20)
+        self.accounts_button.grid(row=2, column=0, pady=10, padx=20)
 
         self.settings_button = CTkButton(
             master=self.navigator, text="Settings", command=self.open_settings
         )
-        self.settings_button.grid(row=2, column=0, pady=10, padx=20)
+        self.settings_button.grid(row=3, column=0, pady=10, padx=20)
 
         # empty row as spacing
-        self.navigator.grid_rowconfigure(3, weight=1)
+        self.navigator.grid_rowconfigure(4, weight=1)
 
         self.about_button = CTkButton(
             master=self.navigator, text="About", command=self.open_about
         )
-        self.about_button.grid(row=4, column=0, pady=(10, 20), padx=20)
+        self.about_button.grid(row=5, column=0, pady=(10, 20), padx=20)
 
-        self.open_news()
+        self.open_instances()
 
     def on_closing(self, event=0):
         self.destroy()
+
+    def open_instances(self):
+        self.main_frame = Instances(master=self)
+        self.main_frame.grid(row=0, column=1, pady=20, padx=20, sticky="nswe")
 
     def open_news(self):
         self.main_frame = News(master=self)
---
+++ ice_launcher/instances.py
@@ -0,0 +1,39 @@
+from customtkinter import CTkFrame, CTkLabel
+from os import listdir, path, makedirs
+
+
+def get_instance_list():
+    # check if instances folder exists
+    if not path.exists("instances"):
+        makedirs("instances")
+
+    return listdir("instances")
+
+
+class Instances(CTkFrame):
+    def __init__(self, master):
+        super().__init__(master=master)
+
+        self.grid_columnconfigure(0, weight=1)
+
+        self.app_name = CTkLabel(
+            master=self,
+            height=100,
+            fg_color=("white", "gray38"),  # <- custom tuple-color
+            text_font=("Roboto Medium", -20),  # font name and size in px
+            text="Instances",
+        )
+        self.app_name.grid(row=0, column=0, pady=20, padx=20, sticky="nswe")
+
+        self.instances = CTkFrame(master=self)
+        self.instances.grid(row=1, column=0, pady=10, padx=10, sticky="nswe")
+
+        self.update_instance_list()
+
+    def update_instance_list(self):
+        for instance in self.instances.winfo_children():
+            instance.destroy()
+
+        for index, instance in enumerate(get_instance_list()):
+            label = CTkLabel(master=self.instances, text=instance)
+            label.grid(row=index, column=0, sticky="nswe")
