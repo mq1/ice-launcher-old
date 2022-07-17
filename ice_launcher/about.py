67ddc01730c30da7a6dc827e6cce6d5740e5669f
Manuel Quarneti <manuelquarneti@gmail.com>
2022-07-17 12:21:28 +0200

    ♻️ Port about from pack to grid

--- ice_launcher/about.py
+++ ice_launcher/about.py
@@ -6,6 +6,8 @@ class About(CTkFrame):
     def __init__(self, master):
         super().__init__(master=master)
 
+        self.grid_columnconfigure(0, weight=1)
+
         self.app_name = CTkLabel(
             master=self,
             height=100,
@@ -13,13 +15,13 @@ class About(CTkFrame):
             text_font=("Roboto Medium", -20),  # font name and size in px
             text="ice-launcher",
         )
-        self.app_name.pack(pady=20, padx=20, fill="x")
+        self.app_name.grid(row=0, column=0, pady=20, padx=20, sticky="nswe")
 
         self.app_version = CTkLabel(master=self, text=__version__)
-        self.app_version.pack(pady=10, padx=20, fill="x")
+        self.app_version.grid(row=1, column=0, pady=20, padx=20)
 
         self.app_author = CTkLabel(master=self, text="Copyright © 2022 Manuel Quarneti")
-        self.app_author.pack(pady=(10, 20), padx=20, fill="x")
+        self.app_author.grid(row=2, column=0, pady=20, padx=20)
 
         self.app_license = CTkLabel(master=self, text="GPL-3.0 Licensed")
-        self.app_license.pack(pady=(10, 20), padx=20, fill="x")
+        self.app_license.grid(row=3, column=0, pady=20, padx=20)
