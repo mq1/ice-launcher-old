6a1075aa7181ef42b391930caed57840e49e87c0
Manuel Quarneti <manuelquarneti@gmail.com>
2022-07-19 23:25:38 +0200

    👷 Re-add pyinstaller

---
+++ build-bundle.py
@@ -0,0 +1,21 @@
+import PyInstaller.__main__
+import os
+import importlib
+
+minecraft_launcher_lib_root = os.path.dirname(importlib.import_module('minecraft_launcher_lib').__file__)  # type: ignore
+version_txt = os.path.join(minecraft_launcher_lib_root, 'version.txt')
+
+customtkinter_root = os.path.dirname(importlib.import_module('customtkinter').__file__)  # type: ignore
+blue_json = os.path.join(customtkinter_root, 'assets/themes/blue.json')
+
+PyInstaller.__main__.run([
+    'ice_launcher/__main__.py',
+    '--name=ice_launcher',
+    '--noconfirm',
+    '--onedir',
+    '--windowed',
+    '--strip',
+    '--noupx',
+    f'--add-data={version_txt}:minecraft_launcher_lib/',
+    f'--add-data={blue_json}:customtkinter/assets/themes/',
+])
