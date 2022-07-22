2c65baed0aaa9b77ed5b0a31f3f626fa4203c1a6
Manuel Quarneti <manuelquarneti@gmail.com>
2022-07-22 18:25:01 +0200

    💄 New Instance window styling

--- ice_launcher/new_instance.py
+++ ice_launcher/new_instance.py
@@ -22,37 +22,32 @@ __instances_dir__: str = path.join(dirs.user_data_dir, "instances")
 class NewInstance(CTkToplevel):
     def __init__(self, master) -> None:
         super().__init__(master=master)
-        self.title = "Add new instance"
+        self.title("Add new instance")
+        self.geometry("250x200")
 
         self.versions = get_available_versions(dirs.user_data_dir)
         self.version_ids = [version["id"] for version in self.versions]
 
         self.grid_columnconfigure(0, weight=1)
 
-        self.app_name = CTkLabel(
-            master=self,
-            height=100,
-            fg_color=("white", "gray38"),  # <- custom tuple-color
-            text_font=("Roboto Medium", -20),  # font name and size in px
-            text="New Instance",
-        )
-        self.app_name.grid(row=0, column=0, pady=20, padx=20, sticky="nswe")
-
         self.instance_name = CTkEntry(master=self, placeholder_text="My new instance")
-        self.instance_name.grid(row=1, column=0, pady=10, padx=10, sticky="nswe")
+        self.instance_name.grid(row=0, column=0, pady=(20, 10), padx=20, sticky="nswe")
 
         self.version = StringVar(value=get_latest_version()["release"])
         self.version_selector = CTkComboBox(
             master=self, values=self.version_ids, variable=self.version
         )
-        self.version_selector.grid(row=2, column=0, pady=10, padx=10, sticky="nswe")
+        self.version_selector.grid(row=1, column=0, pady=10, padx=20, sticky="nswe")
+
+        # empty row as spacing
+        self.grid_rowconfigure(2, weight=1)
 
         self.create_button = CTkButton(
             master=self,
             text="Create",
             command=self.create_instance,
         )
-        self.create_button.grid(row=3, column=0, pady=20, padx=20)
+        self.create_button.grid(row=3, column=0, pady=(10, 20), padx=20)
 
     def create_instance(self) -> None:
         print("Creating instance")
