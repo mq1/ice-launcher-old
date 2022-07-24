da8e9777aad3b3d1df5599c39e6fbae9406c0a6c
Manuel Quarneti <manuelquarneti@gmail.com>
2022-07-24 16:05:12 +0200

    👷 The package for MacOS is now a dmg

---
+++ build-dmg.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+arch="$(uname -m)"
+
+test -f "IceLauncher-macos-${arch}.dmg" && rm "IceLauncher-macos-${arch}.dmg"
+
+create-dmg \
+    --volname "Ice Launcher Installer" \
+    --volicon ice-launcher.png \
+    --icon-size 100 \
+    --icon "Ice Launcher.app" 0 100 \
+    --hide-extension "Ice Launcher.app" \
+    --app-drop-link 200 100 \
+    --no-internet-enable \
+    --format ULMO \
+    "IceLauncher-macos-${arch}.dmg" \
+    dist/Ice\ Launcher.app
