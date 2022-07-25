c5e9869e60f59b91aa370fe83eb92b6f1b758ab5
Manuel Quarneti <manuelquarneti@gmail.com>
2022-07-25 16:20:24 +0200

    ♻️ Refactor build-bundle

--- build-bundle.py
+++ build-bundle.py
@@ -1,21 +1,5 @@
 import PyInstaller.__main__
-import os
-import importlib
-import platform
 
-minecraft_launcher_lib_root = os.path.dirname(importlib.import_module("minecraft_launcher_lib").__file__)  # type: ignore
-version_txt = os.path.join(minecraft_launcher_lib_root, "version.txt")
-minecraft_launcher_lib_data = f"--add-data={version_txt}:minecraft_launcher_lib/"
-if platform.system() == "Windows":
-    minecraft_launcher_lib_data = minecraft_launcher_lib_data.replace("/", "\\")
-    minecraft_launcher_lib_data = minecraft_launcher_lib_data.replace(":minecraft_launcher_lib", ";minecraft_launcher_lib")
-
-customtkinter_root = os.path.dirname(importlib.import_module("customtkinter").__file__)  # type: ignore
-blue_json = os.path.join(customtkinter_root, "assets/themes/blue.json")
-customtkinter_data = f"--add-data={blue_json}:customtkinter/assets/themes/"
-if platform.system() == "Windows":
-    customtkinter_data = customtkinter_data.replace("/", "\\")
-    customtkinter_data = customtkinter_data.replace(":customtkinter", ";customtkinter")
 
 PyInstaller.__main__.run(
     [
@@ -28,7 +12,7 @@ PyInstaller.__main__.run(
         "--strip",
         "--noupx",
         "--icon=ice-launcher.png",
-        minecraft_launcher_lib_data,
-        customtkinter_data,
+        "--collect-data=minecraft_launcher_lib",
+        "--collect-data=customtkinter",
     ]
 )
