e91e876caca128b4a07feafbc8e708d1e6b266d4
Manuel Quarneti <manuelquarneti@gmail.com>
2022-08-09 17:37:12 +0200

    ⚡️ New Instance performance improvements

--- ice_launcher/views/new_instance.py
+++ ice_launcher/views/new_instance.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
+from threading import Thread
+
 from customtkinter import CTkButton, CTkComboBox, CTkEntry, CTkToplevel, StringVar
 from minecraft_launcher_lib.utils import get_latest_version
 
@@ -14,18 +16,13 @@ class NewInstance(CTkToplevel):
         self.title("Add new instance")
         self.geometry("250x200")
 
-        self.versions = versions.get_available()
-        self.version_ids = [version["id"] for version in self.versions]
-
         self.grid_columnconfigure(0, weight=1)
 
         self.instance_name = CTkEntry(master=self, placeholder_text="My new instance")
         self.instance_name.grid(row=0, column=0, pady=(20, 10), padx=20, sticky="nswe")
 
-        self.version = StringVar(value=get_latest_version()["release"])
-        self.version_selector = CTkComboBox(
-            master=self, values=self.version_ids, variable=self.version
-        )
+        self.version = StringVar()
+        self.version_selector = CTkComboBox(master=self, variable=self.version)
         self.version_selector.grid(row=1, column=0, pady=10, padx=20, sticky="nswe")
 
         # empty row as spacing
@@ -37,6 +34,14 @@ class NewInstance(CTkToplevel):
             command=self.create_instance,
         )
         self.create_button.grid(row=3, column=0, pady=(10, 20), padx=20)
+
+        Thread(target=self.update_versions).start()
+
+    def update_versions(self) -> None:
+        av = versions.get_available()
+        self.version_ids = [version["id"] for version in av]
+        self.version_selector.configure(values=self.version_ids)
+        self.version.set(get_latest_version()["release"])
 
     def create_instance(self) -> None:
         instances.new(self.instance_name.get(), self.version.get())
